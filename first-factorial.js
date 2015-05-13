a192acfdd6a75264d327df704c661f31149987a3
GabiOC <oconnor.gabriele@gmail.com>
2015-05-13 11:03:07 -0400

    new solutions

---
+++ first-factorial.js
@@ -0,0 +1,20 @@
+// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range will be between 1 and 18. 
+
+function FirstFactorial(num) { 
+  
+  // if the number is less than 0, reject
+  if(num < 0){
+    return -1;
+  }
+  // if hte number is 0, its factorial is 1
+  else if(num == 0){
+    return 1;
+  }
+  var tmp = num;
+  while(num-- > 2){
+    tmp *= num;
+  }
+  return tmp;        
+}
+
+console.log(FirstFactorial(4)); // example number of 4
---
+++ first-reverse.js
@@ -0,0 +1,8 @@
+//Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
+
+function FirstReverse(str) { 
+  
+  return str.split("").reverse().join("") ;         
+}
+
+console.log(FirstReverse("Example argument"));
