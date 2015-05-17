86da78201b2eac80c6a15b0eb4b0944598c2e0c6
GabiOC <oconnor.gabriele@gmail.com>
2015-05-17 14:54:52 -0400

    Fix powers two solution

--- powers-two.js
+++ powers-two.js
@@ -1,22 +1,22 @@
 /* PROBLEM
 Have the function PowersofTwo(num) take the num parameter being passed which will be an integer and return the string true if
 it's a power of two. If it's not return the string false. For example if the input is 16 then your program should return the string
-true but if the input is 22 then the output should be the string false.
-
-STEPS
-- for loop
-- if i ^ 2 === num, return true (using pow() method)
-- else false */
+true but if the input is 22 then the output should be the string false. */
 
 function powersOfTwo(num){
-	for(i = 0; i < num; i++){
-		if(Math.pow(i, 2) === num){
-			return "true";
-		}
+  if (num === 0)
+    return "true"
+  var i = 2
+    while(i <= num){
+    	if (i === num){
+          return "true"
+    	}
+    	i *= 2;
 	}
-	return "false";
+  return "false";        
 }
 
-console.log(powersOfTwo(8)); // false
+console.log(powersOfTwo(8)); // true
 console.log(powersOfTwo(16)); // true
 console.log(powersOfTwo(22)); // false
+console.log(powersOfTwo(0)); // true
